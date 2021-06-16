e4118e5e94cb52c6b85bc701adb51b59eb865c7f
sean park(park chang heon) <51786976+parkchangheon@users.noreply.github.com>
2021-06-17 03:17:49 +0900

    camera

---
+++ UE4/Character/카메라 시점/tutorial_character.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "tutorial_character.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, tutorial_character, "tutorial_character" );
---
+++ UE4/Character/카메라 시점/tutorial_characterGameModeBase.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "tutorial_characterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUTORIAL_CHARACTER_API Atutorial_characterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	Atutorial_characterGameModeBase();
+	
+};
