75e9fbf09773a8d34c21afac266820369236ba1b
parkchangheon <51786976+parkchangheon@users.noreply.github.com>
2021-09-25 01:41:27 +0900

    c2

---
+++ basic_c++/C++ 학교수업/Circle.h
@@ -0,0 +1,13 @@
+#pragma once
+/*
+class Circle {
+private:
+	int radius;
+public:
+	Circle();
+	Circle(int r);
+	double getArea();
+};
+
+*/
+
---
+++ basic_c++/C++ 학교수업/main.cpp
@@ -0,0 +1,18 @@
+/*
+#include<iostream>
+using namespace std;
+
+#include"Circle.h"
+
+
+int main() {
+	Circle donut;
+	double area = donut.getArea();
+	cout << " µµ³Ó ¸éÀûÀº " << area << endl;
+
+	Circle pizza(30);
+	area = pizza.getArea();
+	cout << " µµ³Ó ¸éÀûÀº " << area << endl;
+
+}
+*/
