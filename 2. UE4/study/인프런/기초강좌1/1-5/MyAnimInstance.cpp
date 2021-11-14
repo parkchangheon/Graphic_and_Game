1316f16e252b59e6b32929488bcb80f072fa0f50
parkchangheon <seanpch@naver.com>
2021-11-15 02:42:37 +0900

    ani

---
+++ 2. UE4/study/인프런/기초강좌1/1-5/MyAnimInstance.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	auto Pawn = TryGetPawnOwner();
+	if (IsValid(Pawn))
+	{
+		Speed = Pawn->GetVelocity().Size();
+	}
+
+	auto Character = Cast<ACharacter>(Pawn);
+		if (Character)
+		{
+			isFalling = Character->GetMovementComponent()->IsFalling();
+
+		}
+}
