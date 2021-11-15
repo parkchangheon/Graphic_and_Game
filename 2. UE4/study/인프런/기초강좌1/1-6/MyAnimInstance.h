e6a78fd4b3ad1cb96814cf8aee2ecc3ad2f0c7b1
parkchangheon <seanpch@naver.com>
2021-11-15 16:45:14 +0900

    inf

---
+++ 2. UE4/study/인프런/기초강좌1/1-6/MyAnimInstance.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyAnimInstance.generated.h"
+
+UCLASS()
+class INFLEARN_UNREAL_API UMyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UMyAnimInstance();
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	void PlayAttackMontage();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Pawn, Meta=(AllowPrivateAccess=true))
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	bool isFalling;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	UAnimMontage* AttackMontage;
+};
