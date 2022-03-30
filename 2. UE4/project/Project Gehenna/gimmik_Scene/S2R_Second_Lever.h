f31a4362261a987d3959d797577d9ded7678908a
park chang heon <51786976+parkchangheon@users.noreply.github.com>
2022-03-30 18:06:02 +0900

    dc

--- 2. UE4/project/Project Gehenna/gimmik_Scene/S2R_Second_Lever.h
+++ 2. UE4/project/Project Gehenna/gimmik_Scene/S2R_Second_Lever.h
@@ -3,33 +3,53 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/TriggerVolume.h"
 #include "S2R_Final_PannelLever.h"
 #include "S2R_Second_Lever.generated.h"
 
-/**
- * 
- */
+DECLARE_DELEGATE(FDelegateTriggerOVL)
+
 UCLASS()
 class TPS_C_PROJECT_API AS2R_Second_Lever : public AS2R_Final_PannelLever
 {
 	GENERATED_BODY()
+
+private:
+	bool IsMoonStand_Lever;
+	bool IsYoolStand_Lever;
 	
 public:
 	AS2R_Second_Lever();
 	virtual void BeginPlay() override;
-	void StartTimer();
-	void AdvanceTimer();   // 타이머가 돌아가면서 호출될 함수
-	void CountdownHasFinished();  //advanceTimer가 지정한 만큼 작동한 뒤에 마무리 처리하기 위함
 
-	FTimerHandle CountdownTimerHandle; // 타이머를 컨트롤 하기 위한 구조체, 카운트다운 종료 후, 타이머 종료 후 돌아가지 않게 하려고
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lever")
+	class UStaticMeshComponent* NPC_Lever;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lever")
+	class UBoxComponent* BoxMesh;
 
 
 public:
-
 	char answer[4] = { 'w','s','d','a' };
-	UPROPERTY(EditAnywhere)
-	int32 CountdownTime;
-	bool isholding;
+
+public:
+	UFUNCTION()
+	void OnBeginOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnCharacterOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+	UFUNCTION()
+	void OnCharacterEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+	UFUNCTION()
+	bool SecondLeverResult(char answer);
+public:
+	FDelegateTriggerOVL DelegateTriggerOVL;
 };
