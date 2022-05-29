700c5084ad790387fd4491875333dfe412e18bff
parkchangheon <51786976+parkchangheon@users.noreply.github.com>
2022-05-29 21:13:09 +0900

    Create 18228.cpp

---
+++ 9.UNIV_Code/1. 인천대학교/2019/18228.cpp
@@ -0,0 +1,44 @@
+//∆Î±œ√ﬂ∂Ù¥Î√•¿ßø¯»∏
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	int* arr = new int[N];
+	int pen=0;  //∆Î±œ¿Ã ¿÷¥¬ ¿Œµ¶Ω∫
+	int right = 0; 
+	int left = 0;
+	int min1=1000000000;
+	int min0 = 1000000000;
+
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		arr[i] = tmp;
+		if (tmp == -1) {
+			pen = i;
+		}
+	}
+	right = left = pen;
+
+	while (right < N || left > 0)
+	{
+		if (right < N)
+		{
+			right++;
+			if (right < N && arr[right] < min1)
+				min1 = arr[right];
+		}
+
+		if (left > 0)
+		{
+			left--;
+			if (arr[left] < min0)
+				min0 = arr[left];
+		}
+	}
+	cout << min0 + min1;
+}
