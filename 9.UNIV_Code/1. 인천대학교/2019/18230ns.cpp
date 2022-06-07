f9081db659f1a0b3d5f6dd0e57db446ae773bd73
parkchangheon <seanpch@naver.com>
2022-06-07 23:54:33 +0900

    Update 18230ns.cpp

--- 9.UNIV_Code/1. 인천대학교/2019/18230ns.cpp
+++ 9.UNIV_Code/1. 인천대학교/2019/18230ns.cpp
@@ -8,54 +8,50 @@
 using namespace std;
 
 int n, a, b;
-int hap=0;
-int cnt = 0;
+int hap = 0;
 vector<int>va;
 vector<int>vb;
 
-void next(int tmp_count, int s)
+void next(int tmp_count, int cnt, int s)
 {
-	if (cnt > n)
+	if (cnt >= n)
 	{
 		if (s >= hap)
 			hap = s;
-		return; 
+		return;
 	}
 
 	if (tmp_count == 1) // 1일때
 	{
 		if (!va.empty())
 		{
-			s += va[va.size()-1];
+			s += va[va.size() - 1];
 			va.pop_back();
 		}
-		cnt += 1;
 	}
 
-	else if (tmp_count == 2) //2가지 경우가 가능
+	else if (tmp_count == 2) //2일때 2가지를 넣어준다.
 	{
-		if (va[va.size() - 1] + va[va.size() - 2] > vb[vb.size() - 1])
-		{
-			s += (va[va.size() - 1] + va[va.size() - 2]);
-			va.pop_back();
-			va.pop_back();
+		if (va.size() >= 2) { //만약 a 벡터의 값이 2개 이상 존재할때
+			if (va[va.size() - 1] + va[va.size() - 2] > vb[vb.size() - 1])
+			{
+				s += (va[va.size() - 1] + va[va.size() - 2]);
+				va.pop_back();
+				va.pop_back();
+			}
+			else {
+				s += vb[vb.size() - 1];
+				vb.pop_back();
+			}
 		}
-		else {
+		else if (vb.size() > 0) { //a 벡터의 값이 2개 이상 존재하지 않을때
 			s += vb[vb.size() - 1];
 			vb.pop_back();
 		}
-		cnt += 2;
 	}
-	for (int i = 0; i < va.size(); i++)
-		cout << va[i] << ' ';
-	cout << endl;
-	for (int i = 0; i < vb.size(); i++)
-		cout << vb[i] << ' ';
-	cout << endl;
 
-
-	next(1, s);
-	next(2, s);
+	next(1, cnt + 1, s);
+	next(2, cnt + 2, s);
 }
 
 
@@ -85,7 +81,9 @@ int main()
 	//2*1 이랬지만, 1*2도 가능
 	// DP 문제 
 
-	next(0, 0);
-	cout<<hap;
-	
+	next(0, 0, 0);
+	cout << hap;
+
 }
+
+
