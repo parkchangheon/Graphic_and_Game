856284722d3b2d64e293e77cf2c33cef6e2ae824
parkchangheon <seanpch@naver.com>
2022-07-05 01:23:41 +0900

    Create EnemyPatrol.cs

---
+++ 6. Projects/2DPlatformer/Begin_Pixel_2D/Enemy&Trap/EnemyPatrol.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+
+}
