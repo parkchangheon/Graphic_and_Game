941726a3a58fdb246faf2d5f570d2c7a7e5c54ce
parkchangheon <seanpch@naver.com>
2022-07-06 23:44:14 +0900

    Update EnemyPatrol.cs

--- 6. Projects/2DPlatformer/Begin_Pixel_2D/Enemy&Trap/EnemyPatrol.cs
+++ 6. Projects/2DPlatformer/Begin_Pixel_2D/Enemy&Trap/EnemyPatrol.cs
@@ -14,15 +14,50 @@ public class EnemyPatrol : MonoBehaviour
 
     [Header("Movement Parameter")]
     [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingleft;
+
+    private void Awake()
+    {
+        initScale = enemy.localScale;
+    }
 
     private void Update()
     {
-        MoveInDirection(1);
+        if (movingleft)
+        {
+            if (enemy.position.x >= leftEdge.position.x)
+                MoveInDirection(1);
+            else
+            {
+                //change direction
+                DirectionChange();
+            }
+        }
+
+        else
+        {
+            if(enemy.position.x <= leftEdge.position.x)
+                MoveInDirection(-1);
+            else
+            {
+                //change direction
+                DirectionChange();
+            }
+        }
     }
+
+    private void DirectionChange()
+    {
+        movingleft = !movingleft;
+    }
+
+
 
     private void MoveInDirection(int _direction)
     {
         //Make enemy face direction
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction * -1, initScale.y, initScale.z);
 
         //Move in direction
         enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, enemy.position.y, enemy.position.z);
