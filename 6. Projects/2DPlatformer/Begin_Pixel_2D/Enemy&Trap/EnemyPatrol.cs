2c132dccb1777882a4d79824aa9dc3e20399a762
parkchangheon <seanpch@naver.com>
2022-07-06 23:25:02 +0900

    Update EnemyPatrol.cs

--- 6. Projects/2DPlatformer/Begin_Pixel_2D/Enemy&Trap/EnemyPatrol.cs
+++ 6. Projects/2DPlatformer/Begin_Pixel_2D/Enemy&Trap/EnemyPatrol.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
+    [Header("Patrol Points")]
     [SerializeField] private Transform leftEdge;
     [SerializeField] private Transform rightEdge;
 
 
+    [Header("enemy")]
+    [SerializeField] private Transform enemy;
+
+    [Header("Movement Parameter")]
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        MoveInDirection(1);
+    }
+
+    private void MoveInDirection(int _direction)
+    {
+        //Make enemy face direction
+
+        //Move in direction
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, enemy.position.y, enemy.position.z);
+    }
 }
