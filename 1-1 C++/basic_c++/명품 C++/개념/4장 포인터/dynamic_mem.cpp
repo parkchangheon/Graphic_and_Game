ec7f8e107a8f0ecb9648cf583c7c590e56792a93
parkchangheon <seanpch@naver.com>
2022-07-11 03:07:21 +0900

    Update dynamic_mem.cpp

--- 1-1 C++/basic_c++/명품 C++/개념/4장 포인터/dynamic_mem.cpp
+++ 1-1 C++/basic_c++/명품 C++/개념/4장 포인터/dynamic_mem.cpp
@@ -77,44 +77,151 @@
 //	delete p;
 //}
 
-class Circle {
-	int r;
-public:
-	Circle() {};
-	Circle(int r) {};
-};
+//class Circle {
+//	int r;
+//public:
+//	Circle() {};
+//	Circle(int r) {};
+//};
 
 
 
 
 
-// 배열의 동적 할당 및 반환
-//자료형 *포인터변수 =  new 자료형[배열의 크기]
-//delete[] 포인터변수
+//// 배열의 동적 할당 및 반환
+////자료형 *포인터변수 =  new 자료형[배열의 크기]
+////delete[] 포인터변수
+//#include<iostream>
+//using namespace std;
+//int main() {
+//
+//	////배열의 초기화는 불가능하다.
+//	////int* Array = new int[10](20);
+//	//int* Array = new int[] { 10, 20, 30, 40 }; //하지만 이와같이 초기화는 가능하다.
+//
+//	//int *arr = new int[3];
+//	//if (!arr)
+//	//	return;
+//
+//	//for (int i = 0; i < 3; i++) {
+//	//	cout << arr[i] << endl;
+//	//}
+//
+//	//delete[] arr;
+//
+//	Circle* ArrCircle = new Circle;   //생성자 Circle() 호출
+//	Circle* ArrCircle2 = new Circle(30); // 생성자 Cricle(30) 호출
+//
+//	delete ArrCircle;
+//	delete ArrCircle2;
+//
+//
+//	
+//}
+
+
+
+//#include<iostream>
+//using namespace std;
+//
+//class Circle {
+//public:
+//	Circle() {};
+//	Circle(int r) {};
+//};
+//
+//int main()
+//{
+//	Circle* p = new Circle;
+//	Circle* q = new Circle(30);
+//
+//	delete p;
+//	delete q;
+//
+//}
+
+
+
 #include<iostream>
 using namespace std;
-int main() {
 
-	////배열의 초기화는 불가능하다.
-	////int* Array = new int[10](20);
-	//int* Array = new int[] { 10, 20, 30, 40 }; //하지만 이와같이 초기화는 가능하다.
+class Circle {
+	int radius;
 
-	//int *arr = new int[3];
-	//if (!arr)
-	//	return;
+public:
+	Circle();
+	Circle(int r);
+	~Circle();
+	void setRadius(int r) { radius = r; }
+	double getArea() { return 3.14 * radius * radius; }
+};
 
-	//for (int i = 0; i < 3; i++) {
-	//	cout << arr[i] << endl;
-	//}
-
-	//delete[] arr;
-
-	Circle* ArrCircle = new Circle;   //생성자 Circle() 호출
-	Circle* ArrCircle2 = new Circle(30); // 생성자 Cricle(30) 호출
-
-	delete ArrCircle;
-	delete ArrCircle2;
-
-
-	
+Circle::Circle() {
+	radius = 1;
+	cout << "생성자 실행 radius = " << radius << endl;
 }
+
+
+Circle::Circle(int r) {
+	this->radius = r;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::~Circle()
+{
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+//
+//
+//
+//int main()
+//{
+//	int radius;
+//	while (true) {
+//		cout << "정수 반지름 입력(음수이면 종료) >> ";
+//		cin >> radius;
+//		if (radius < 0)
+//			break;
+//		Circle* p = new Circle(radius);
+//		cout << "원의 면적은 = " << p->getArea() << endl;
+//		delete p;
+//	}
+//}
+
+
+
+
+//int main() {
+//	Circle* pArray = new Circle[3];
+//	Circle* pBrray = new Circle[]{Circle(3), Circle(2), Circle(1)};
+//
+//	for (int i = 0; i < 3; i++)
+//		cout << pBrray[i].getArea() << endl;
+//
+//	Circle* p = pBrray;
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cout << p->getArea() << endl;
+//		p++;
+//	}
+//	delete[]pBrray;
+//}
+
+
+//메모리 누수에 관하여
+
+//int main()
+//{
+//	char n = 'a';
+//	char* p = new char[1024];
+//	p = &n;
+//	//이렇게 된다면, new로 할당 받은 1024바이트는 메모리 누수가 발생한다.
+//	//p가 처음에는 할당받은 1024바이트를 n을 받게되면서 잃어버려 반환할 수 없게 된다.
+//
+//	for (int i = 0; i < 1000000; i++)
+//	{
+//		p = new char[1024];
+//	}
+//}
+
+
