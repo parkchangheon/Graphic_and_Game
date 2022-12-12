79d29e805d51f15e10eef0157279a4d0f11e09ee
parkchangheon <seanpch@naver.com>
2022-12-12 23:28:49 +0900

    second

---
+++ 6. Projects/Practice_Project/Udemy/Unity_Udemy_01/Assets/Assault_Fly/Script/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalThrow);
+
+        float verticalThrow = Input.GetAxis("Vertical");
+        Debug.Log(verticalThrow);
+    }
+}
