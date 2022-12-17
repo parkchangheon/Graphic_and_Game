ade9b4746335cfab5af456031cbbf4ac30dbe020
parkchangheon <seanpch@naver.com>
2022-12-17 16:59:27 +0900

    defense

--- 6. Projects/Practice_Project/Udemy/Defense_Project/Defense_Game/Assets/script/EnemyMover.cs
+++ 6. Projects/Practice_Project/Udemy/Defense_Project/Defense_Game/Assets/script/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     void Start()
     {
@@ -16,9 +16,17 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(WayPoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
-            
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition); //진행하고 있는 방향을 바라보도록 처리
+
+            while(travelPercent < 1f) { //0~1로 셋업
+                travelPercent += (Time.deltaTime * speed); //1프레임 움직임
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent); // 선형 보간 -> 시작점, 끝점, 어느 지점에 위치할지.
+                yield return new WaitForEndOfFrame(); //1프레임 끝날때, yield return
+            }
         }
     }
 }
--- 6. Projects/Practice_Project/Udemy/Defense_Project/Defense_Game/Assets/script/WayPoint.cs
+++ 6. Projects/Practice_Project/Udemy/Defense_Project/Defense_Game/Assets/script/WayPoint.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject towerPrefab;
+    [SerializeField] bool isPlaceable;
+
+    void OnMouseDown()
     {
-        
+        if(isPlaceable)
+        {
+            Instantiate(towerPrefab, transform.position, Quaternion.identity); //게임 실행 도중에 오브젝트를 생성할 수 있다. (생성하고자 하는 오브젝트, 위치, 회전값 지정 default identity)
+            isPlaceable = false;
+        }
+            
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
 }
