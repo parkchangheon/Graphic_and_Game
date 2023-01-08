5f1a352fa0e1b792c278ac4f933531393926b98b
parkchangheon <51786976+parkchangheon@users.noreply.github.com>
2023-01-08 15:20:01 +0900

    using&&enum

---
+++ 1-1 C++/Re_C++/8_MODERN_C++/5_ENUM.cpp
@@ -0,0 +1,42 @@
+#include<list>
+#include<deque>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+//unscopped enum (범위없는 enum) 다른 공간에서 아래 열거형을 사용할 수 없게된다.
+enum PlayerType
+{
+	Knight,
+	Archer,
+	Mage,
+};
+
+enum MonsterType
+{
+	MS_Knight, //이름이 겹친다.(MS로 변경)
+};
+
+//위의 문제를 해결하기 위해 아래와 같이 enum class로 만들어준다.
+//이름공간 관리가 편하나, 암묵적 변환이 되질 않아 일일히 casting해줘야함...
+enum class ObjectType
+{
+	Player,
+	Monster,
+	Projectile,
+};
+
+int main()
+{
+	double value = Knight; //일반적인 enum 활용법 (암묵적 변환)
+	//double value2 = ObjectType::Player; //암묵적인 변환 x 
+	double value2 = static_cast<double>(ObjectType::Player); //암묵적인 변환 x 
+
+
+	return 0;
+}
