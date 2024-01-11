9560a8661976564e23cca7f176d385ec5adcd51e
parkchangheon <seanpch@naver.com>
2024-01-11 23:48:29 +0900

    AnimInstance_COde

--- Unreal_C++/Animation/ABAnimInstance.cpp
+++ Unreal_C++/Animation/ABAnimInstance.cpp
@@ -2,4 +2,40 @@
 
 
 #include "Animation/ABAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+UABAnimInstance::UABAnimInstance()
+{
+	MovingThreshold = 3.0f;
+	JumpingThreshold = 100.0f;
+}
+
+void UABAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Owner = Cast<ACharacter>(GetOwningActor());
+	if (Owner)
+	{
+		Movement = Owner->GetCharacterMovement();
+	}
+
+}
+
+void UABAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Movement)
+	{
+		Velocity = Movement->Velocity;
+		GroundSpeed = Velocity.Size2D();
+		bIsIdle = GroundSpeed < MovingThreshold;
+		bIsFalling = Movement->IsFalling();
+		bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshold);
+
+	}
+
+
+}
--- Unreal_C++/Animation/ABAnimInstance.h
+++ Unreal_C++/Animation/ABAnimInstance.h
@@ -14,4 +14,37 @@ class TESTTT_API UABAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+	UABAnimInstance();
+
+protected:
+	virtual void NativeInitializeAnimation()override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds)override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Character)
+	TObjectPtr<class ACharacter> Owner;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Character)
+	TObjectPtr<class UCharacterMovementComponent>Movement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Character)
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Character)
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Character)
+	uint8 bIsIdle : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Character)
+	float MovingThreshold;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsFalling : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Character)
+	uint8 bIsJumping : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Character)
+	float JumpingThreshold;
 };
